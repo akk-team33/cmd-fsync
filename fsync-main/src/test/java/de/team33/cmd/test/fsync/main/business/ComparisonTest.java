dd8c4a4f1098e5328e7dbbba388df0378900464e
Andreas Kluge-Kaindl <akk-pg@team33.de>
2023-11-17 15:49:17 +0100

    draft implementation Comparision and ComparisionTest (still dirty)

--- fsync-main/src/test/java/de/team33/cmd/test/fsync/main/business/ComparisonTest.java
+++ fsync-main/src/test/java/de/team33/cmd/test/fsync/main/business/ComparisonTest.java
@@ -2,16 +2,15 @@ package de.team33.cmd.test.fsync.main.business;
 
 import de.team33.cmd.fsync.main.business.Comparison;
 import de.team33.cmd.fsync.main.common.Context;
-import de.team33.patterns.testing.titan.io.Redirected;
+import de.team33.patterns.io.deimos.TextIO;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
 
 import static de.team33.patterns.testing.titan.io.Redirected.outputOf;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ComparisonTest implements Context {
 
@@ -19,12 +18,14 @@ class ComparisonTest implements Context {
     void run_both_are_missing() throws IOException {
         final Path left = Path.of("left", "is", "missing");
         final Path right = Path.of("right", "is", "missing");
+        final String expected = String.format(TextIO.read(getClass(), "ComparisonTest.both_are_missing.txt"),
+                                              left, right);
 
         final String result = outputOf(() -> Comparison.runnable(this,
                                                                  getClass().getSimpleName(),
                                                                  List.of(left.toString(), right.toString()))
                                                        .run());
 
-        assertEquals("", result);
+        assertEquals(expected, result);
     }
 }
